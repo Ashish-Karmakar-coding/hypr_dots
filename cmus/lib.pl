3d271bd035b0449912447ff37c4eb3472fbee7f5
Ashish Karmakar <ashish.karmakar782@gmail.com>
2026-06-14 18:10:36 +0530

    some changes

---
+++ cmus/lib.pl
@@ -0,0 +1,11 @@
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Do I Wanna Know.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/R U Mine.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/One For The Road.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/I Want It All.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/No. 1 Party Anthem.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Mad Sounds.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Fireside.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Why'd You Only Call Me When You're High.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Snap Out Of It.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/Knee Socks.flac
+/mnt/SONG_Library/English Songs/Arctic Monkeys/I Wanna Be Yours.flac
